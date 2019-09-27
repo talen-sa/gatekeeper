fdefb704be71ee10b89f339a2c12fb2b2837df9e
michaelvanbosch <mvanbosch@sourceallies.com>
2019-09-27 15:42:43 -0500

    test

--- slack_api/src/controllers/messageController.js
+++ slack_api/src/controllers/messageController.js
@@ -71,7 +71,7 @@ const openDeleteDialog = async (trigger_id) => {
             elements: [
                 {
                     type: 'select',
-                    initial_option: 'Teams',
+                    initial_option: '1',
                     name: 'team',
                     label: 'Choose the team to delete',
                     data_source: 'external',
