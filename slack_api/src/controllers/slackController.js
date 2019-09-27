c37308cd9af036b5a4b55f14240b7e618d61e962
michaelvanbosch <mvanbosch@sourceallies.com>
2019-09-27 13:56:48 -0500

    change api endpoint

--- slack_api/src/controllers/slackController.js
+++ slack_api/src/controllers/slackController.js
@@ -6,5 +6,5 @@ module.exports.getUsersInOrganization = async function() {
     const payload = {
         token: process.env.SLACK_ACCESS_TOKEN,
     };
-    return axios.post(`${apiUrl}/dialog.open`, qs.stringify(payload));
+    return axios.post(`${apiUrl}/users.list`, qs.stringify(payload));
 };
