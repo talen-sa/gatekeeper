1e781d59d2e14bb17273deabef61713e6491a37c
michaelvanbosch <mvanbosch@sourceallies.com>
2019-09-27 16:32:27 -0500

    help '

--- slack_api/src/controllers/commandsController.js
+++ slack_api/src/controllers/commandsController.js
@@ -46,7 +46,7 @@ let handleEvents = async function(req, res) {
             const { user_id, trigger_id } = req.body;
             try {
                 const result = await message.openRemoveUserDialog(trigger_id);
-                console.log('deb',result);
+                console.log('deb',result.data.response_metadata);
                 if (result.data.error) {
                     res.sendStatus(500);
                 } else {
