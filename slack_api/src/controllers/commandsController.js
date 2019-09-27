e048655f541a36f4c5d1b6d9493482f2b4697dc3
michaelvanbosch <mvanbosch@sourceallies.com>
2019-09-27 15:08:29 -0500

    fix

--- slack_api/src/controllers/commandsController.js
+++ slack_api/src/controllers/commandsController.js
@@ -40,25 +40,44 @@ let handleEvents = async function(req, res) {
             }
         }
     }
-    if (req.body.command === '/leave_team') {
-        if (!signature.isVerified(req)) {
-            res.sendStatus(404);
-            return;
-        } else {
-            const { user_id, trigger_id } = req.body;
-            try {
-                const result = await message.openDialog(trigger_id);
-                if (result.data.error) {
-                    res.sendStatus(500);
-                } else {
-                    //teamService.createTeam(data);
-                    res.send('');
-                }
-            } catch (err) {
-                res.sendStatus(500);
-            }
-        }
-    }
+    // if (req.body.command === '/leave_team') {
+    //     if (!signature.isVerified(req)) {
+    //         res.sendStatus(404);
+    //         return;
+    //     } else {
+    //         const { user_id, trigger_id } = req.body;
+    //         try {
+    //             const result = await message.openDialog(trigger_id);
+    //             if (result.data.error) {
+    //                 res.sendStatus(500);
+    //             } else {
+    //                 //teamService.createTeam(data);
+    //                 res.send('');
+    //             }
+    //         } catch (err) {
+    //             res.sendStatus(500);
+    //         }
+    //     }
+    // }
+    // if (req.body.command === '/join_team') {
+    //     if (!signature.isVerified(req)) {
+    //         res.sendStatus(404);
+    //         return;
+    //     } else {
+    //         const { user_id, trigger_id } = req.body;
+    //         try {
+    //             const result = await message.openDialog(trigger_id);
+    //             if (result.data.error) {
+    //                 res.sendStatus(500);
+    //             } else {
+    //                 //teamService.createTeam(data);
+    //                 res.send('');
+    //             }
+    //         } catch (err) {
+    //             res.sendStatus(500);
+    //         }
+    //     }
+    // }
     else if (req.body.command === '/in') {
         if (!signature.isVerified(req)) {
             res.sendStatus(404);
