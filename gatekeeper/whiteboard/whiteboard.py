99b0aab387963444c52b1e25e085ada736d2b730
Stephen Onnen <stephen.onnen@gmail.com>
2019-09-27 17:53:43 -0500

    Updated whiteboard

--- gatekeeper/whiteboard/whiteboard.py
+++ gatekeeper/whiteboard/whiteboard.py
@@ -1,19 +1,18 @@
-import time
 import board
 import neopixel
 
 _pixels = neopixel.NeoPixel(board.D12, 1)
 
-_colors = {
-    'red': (10, 0, 0),
-    'green': (0, 10, 0),
-    'blue': (0, 0, 10)
-}
+_colors = {"red": (10, 0, 0), "green": (0, 10, 0), "blue": (0, 0, 10)}
+
 
 class WhiteboardError(Exception):
     pass
 
+
 def set_status(position, status):
     if position > len(_pixels):
-        raise WhiteboardError("position {} exceeds row count {}", position, len(_pixels))
-    _pixels[position] = _colors['red'] if status == 0 else _colors['green']
+        raise WhiteboardError(
+            "position {} exceeds row count {}", position, len(_pixels)
+        )
+    _pixels[position] = _colors["red"] if status == 0 else _colors["green"]
