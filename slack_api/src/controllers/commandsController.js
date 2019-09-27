04485d2f0a4195d50fd5a6e71ade804858f19b97
michaelvanbosch <mvanbosch@sourceallies.com>
2019-09-27 17:32:51 -0500

    stringify:

--- slack_api/src/controllers/commandsController.js
+++ slack_api/src/controllers/commandsController.js
@@ -130,7 +130,7 @@ let handleEvents = async function(req, res) {
             try {
                 let result = await teamService.getAllTeamsStatus();
                 result = json2array(result);
-                console.log(result);
+                console.log(result.toString());
                 message.sendShortMessage(user_id, `Who's Here:` + result.toString().replace(/[,]/g, ""));
                 res.send('');
             } catch (e) {
