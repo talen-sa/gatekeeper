25c18c2b727f09fa62c60624771ffc2db6c36413
Stephen Onnen <stephen.onnen@gmail.com>
2019-09-28 15:56:38 -0500

    translate team position to led position

--- gatekeeper/whiteboard/__init__.py
+++ gatekeeper/whiteboard/__init__.py
@@ -4,6 +4,8 @@ from gatekeeper.config import Config
 
 _pixels = [{}] * Config.ROW_COUNT
 
+_leds = []
+
 if Config.USE_BOARD is not None:
     import board
     import neopixel
@@ -22,17 +24,26 @@ class WhiteboardStatus(Enum):
     IN = 1
 
 
+def _translate_position(index):
+    num_leds = len(_pixels)
+    if index < num_leds:
+        return num_leds - (1 + index)
+    else:
+        return index - (num_leds / 2)
+
+
 def set_status(position, status):
     if position >= len(_pixels):
         raise WhiteboardError(
             "position {} exceeds row count {}".format(position, len(_pixels))
         )
+    led_position = _translate_position(position)
     if status == WhiteboardStatus.OUT.value:
-        _pixels[position] = _colors["red"]
+        _pixels[led_position] = _colors["red"]
     elif status == WhiteboardStatus.IN.value:
-        _pixels[position] = _colors["green"]
+        _pixels[led_position] = _colors["green"]
     else:
-        _pixels[position] = _colors["blue"]
+        _pixels[led_position] = _colors["blue"]
 
 
 def toggle_status(status):
