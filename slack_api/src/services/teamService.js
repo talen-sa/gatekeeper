8905b31cc6acef43f2ee1f844b80d322d52cd33b
michaelvanbosch <mvanbosch@sourceallies.com>
2019-09-28 09:34:53 -0500

    test

--- slack_api/src/services/teamService.js
+++ slack_api/src/services/teamService.js
@@ -47,6 +47,7 @@ module.exports.getAllTeamsStatus = async function() {
   });
 }
 module.exports.createTeam = async function(team_name) {
+  console.log(team_name);
   return new Promise(function(resolve, reject) {
     axios.post(PI_API_URL + '/teams/', {
       name: team_name
