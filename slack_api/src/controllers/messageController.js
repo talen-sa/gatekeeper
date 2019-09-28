645b39adf68e89dc415c9c244d668e6deeececa2
michaelvanbosch <mvanbosch@sourceallies.com>
2019-09-28 16:03:59 -0500

    asd

--- slack_api/src/controllers/messageController.js
+++ slack_api/src/controllers/messageController.js
@@ -27,7 +27,7 @@ const send = async (data) => {
 const openCreateTeamDialog = async (trigger_id) => {
     let open_positions = await teamService.getOpenBoardPositions();
     let open_positions_str = open_positions.toString();
-    console.log(open_positions_str);
+    console.log(open_positions);
 
     const dialogData = {
         token: process.env.SLACK_ACCESS_TOKEN,
