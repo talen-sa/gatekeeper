ffa5e5fbb64489c992bc45b0d244f71fb5ef118e
michaelvanbosch <mvanbosch@sourceallies.com>
2019-09-28 11:35:30 -0500

    test

--- slack_api/src/services/teamService.js
+++ slack_api/src/services/teamService.js
@@ -134,14 +134,17 @@ module.exports.removeUserFromTeam = async function(user, team) {
     });
   });
 }
+let getUsernameFromSlack = async function(user_id) {
+  return await slackController.getUserById(member.username);
+}
 module.exports.listUsersOnTeam = async function(team) {
   return new Promise(function(resolve, reject) {
     axios.get(PI_API_URL + '/teams/' + team)
     .then(function (response) {
       let result = [];
       for (var member of response.data.data.members) {
+        let user = getUsernameFromSlack(user_id);
         
-        let user = await slackController.getUserById(member.username);
         result.push({name:user});
       }
         resolve(result);
