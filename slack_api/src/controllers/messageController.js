acb6b712a21f9ced0c750a2ee84ca82bbd55b057
michaelvanbosch <mvanbosch@sourceallies.com>
2019-09-28 15:49:55 -0500

    asd

--- slack_api/src/controllers/messageController.js
+++ slack_api/src/controllers/messageController.js
@@ -25,6 +25,14 @@ const send = async (data) => {
 };
 
 const openCreateTeamDialog = async (trigger_id) => {
+    let teams = await teamService.getTeams();
+    let taken_positions = [];
+    for (var a = 0; a < teams.length; a++) {
+        taken_positions.push(teams[a].board_position)
+    }
+    console.log(taken_positions);
+    let open_positions = [];
+
     const dialogData = {
         token: process.env.SLACK_ACCESS_TOKEN,
         trigger_id: trigger_id,
@@ -50,7 +58,7 @@ const openCreateTeamDialog = async (trigger_id) => {
                     type: 'text',
                     name: 'board_position',
                     placeholder: '0',
-                    label: 'Board Location (0-19)',
+                    label: `Board Location\n Available Positions: ${open_positions})`,
                 }
             ]
         })
