cd18c872b9030fa0825d58a3b89a7947500e77aa
michaelvanbosch <mvanbosch@sourceallies.com>
2019-09-28 11:45:44 -0500

    test

--- slack_api/src/services/teamService.js
+++ slack_api/src/services/teamService.js
@@ -142,7 +142,7 @@ module.exports.listUsersOnTeam = async function(team) {
       let result = [];
       for (var member of response.data.data.members) {
         let user = await slackController.getUserById(member.username);
-        result.push({name:user});
+        result.push({name:user.real_name});
       }
         resolve(result);
       }).catch(function (error) {
