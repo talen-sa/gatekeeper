8c468c652a3f07f8b407c038cf8a00280748c4c1
michaelvanbosch <mvanbosch@sourceallies.com>
2019-09-28 14:44:12 -0500

    test

--- slack_api/src/controllers/interactionsController.js
+++ slack_api/src/controllers/interactionsController.js
@@ -55,7 +55,7 @@ let handleInteractions = async function(req, res) {
                     }
                     console.log(result);
                     if (result.length != 0) {
-                        message.sendShortMessage(user.id, `*The teammates on team ${submission.team} are:*\n` + '`' + formattedList.toString().replace(/[,]/g, "") + '`');
+                        message.sendShortMessage(user.id, `*The teammates on team ${submission.team} are:*\n` +  formattedList.toString().replace(/[,]/g, ""));
                     }
                     else {
                         message.sendShortMessage(user.id, `*The team \'${submission.team}\' has no teammates yet.*`);
