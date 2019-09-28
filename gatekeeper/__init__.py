150f62669af07dc443710afbfc258f41f399d140
Stephen Onnen <stephen.onnen@gmail.com>
2019-09-28 16:54:12 -0500

    fixed

--- gatekeeper/__init__.py
+++ gatekeeper/__init__.py
@@ -15,16 +15,15 @@ register_blueprints(app)
 db.init_app(app)
 ma.init_app(app)
 
-toggle_status(2)
-
-for i in range(Config.ROW_COUNT):
-    team = Team.get_team_at_position(i)
-    if team is not None:
-        set_status(i, team.status)
-
 
 @app.before_first_request
 def init_forum():
+    toggle_status(2)
+
+    for i in range(Config.ROW_COUNT):
+        team = Team.get_team_at_position(i)
+        if team is not None:
+            set_status(i, team.status)
     base.metadata.create_all(bind=db.engine)
 
 
