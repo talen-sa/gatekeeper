8e08bf51bfb428b1ad05fd7e1fbfb2f9fff8c9e0
michaelvanbosch <mvanbosch@sourceallies.com>
2019-09-29 14:27:20 -0500

    rename some commands

--- slack_api/src/controllers/commandsController.js
+++ slack_api/src/controllers/commandsController.js
@@ -2,259 +2,265 @@ const message = require('./messageController');
 const signature = require('../verifySignature');
 const teamService = require('../services/teamService')
 
+
+let checkSlackSiganature = function(req, res) {
+    if (!signature.isVerified(req)) {
+        res.sendStatus(404);
+        return false;
+    }
+    return true;
+}
 let handleEvents = async function(req, res) {
-    if (req.body.command === '/create_team') {
-        if (!signature.isVerified(req)) {
-            res.sendStatus(404);
-            return;
-        } else {
-            const { user_id, trigger_id } = req.body;
-            try {
-                const result = await message.openCreateTeamDialog(trigger_id);
-                if (result.data.error) {
-                    res.sendStatus(500);
-                } else {
-                    res.send('');
-                }
-            } catch (err) {
-                res.sendStatus(500);
-            }
-        }
-    }
-    if (req.body.command === '/delete_team') {
-        if (!signature.isVerified(req)) {
-            res.sendStatus(404);
-            return;
-        } else {
-            const { user_id, trigger_id } = req.body;
-            try {
-                const result = await message.openDeleteTeamDialog(trigger_id);
-                if (result.data.error) {
-                    res.sendStatus(500);
-                } else {
-                    res.send('');
-                }
-            } catch (err) {
-                res.sendStatus(500);
-            }
-        }
-    }
-    if (req.body.command === '/remove_user') {
-        if (!signature.isVerified(req)) {
-            res.sendStatus(404);
-            return;
-        } else {
-            const { user_id, trigger_id } = req.body;
-            try {
-                const result = await message.openRemoveUserDialog(trigger_id);
-                if (result.data.error) {
-                    res.sendStatus(500);
-                } else {
-                    res.send('');
-                }
-            } catch (err) {
-                res.sendStatus(500);
-            }
-        }
-    }
-    if (req.body.command === '/add_user') {
-        if (!signature.isVerified(req)) {
-            res.sendStatus(404);
-            return;
-        } else {
-            const { user_id, trigger_id } = req.body;
-            try {
-                const result = await message.openAddUserDialog(trigger_id);
-                if (result.data.error) {
-                    res.sendStatus(500);
-                } else {
-                    res.send('');
-                }
-            } catch (err) {
-                res.sendStatus(500);
-            }
-        }
-    }
-    if (req.body.command === '/list_teams') {
-        if (!signature.isVerified(req)) {
-            res.sendStatus(404);
-            return;
-        } else {
-            const { user_id, trigger_id } = req.body;
-            try {
-                const result = await message.openListUsersOnTeamDialog(trigger_id);
-                if (result.data.error) {
-                    res.sendStatus(500);
-                } else {
-                    res.send('');
-                }
-            } catch (err) {
-                res.sendStatus(500);
-            }
-        }
-    }
-    if (req.body.command === '/update_team_position') {
-        if (!signature.isVerified(req)) {
-            res.sendStatus(404);
-            return;
-        } else {
-            const { user_id, trigger_id } = req.body;
-            try {
-                const result = await message.openUpdateTeamDialog(trigger_id);
-                if (result.data.error) {
-                    res.sendStatus(500);
-                } else {
-                    res.send('');
-                }
-            } catch (err) {
-                res.sendStatus(500);
-            }
-        }
-    }
-    if (req.body.command === '/my_teams') {
-        if (!signature.isVerified(req)) {
-            res.sendStatus(404);
-            return;
-        } else {
-            const { user_id, trigger_id } = req.body;
-            try {
-                const teamNames = await teamService.getMyTeamNames(user_id);
-                message.sendShortMessage(user_id, `*Your teams are:*\n` +  teamNames.toString().replace(/[,]/g, "\n"));
-                res.send('');
-                console.log(result);
-                if (result.data.error) {
-                    res.sendStatus(500);
-                } else {
-                    res.send('');
-                }
-            } catch (err) {
-                res.sendStatus(500);
-            }
-        }
-    }
-    if (req.body.command === '/update_team_status') {
-        if (!signature.isVerified(req)) {
-            res.sendStatus(404);
-            return;
-        } else {
-            const { user_id, trigger_id } = req.body;
-            try {
-                message.sendShortMessage(user_id, `Nobody is here.`);
-                if (result.data.error) {
-                    res.sendStatus(500);
-                } else {
-                    res.send('');
-                }
-            } catch (err) {
-                res.sendStatus(500);
-            }
-        }
-    }
-    if (req.body.command === '`/who`s_here') {
-        if (!signature.isVerified(req)) {
-            res.sendStatus(404);
-            return;
-        } else {
-            const { user_id, trigger_id } = req.body;
-            try {
-                let result = await teamService.getAllTeamsStatus();
-                let empty = true;
-                let msgList = [];
-                msgList.push(`*Whos's here?*\n`);
-                for (var a = 0; a < result.teams.length; a++) {
-                    if (result.teams[a].status =='1') {
-                        msgList.push(`\`${result.teams[a].team}\``);
-                        empty = false;
+    if (checkSlackSiganature(req, res)) {
+        if (req.body.command === '/create_team') {
+            if (!signature.isVerified(req)) {
+                res.sendStatus(404);
+                return;
+            } else {
+                const { user_id, trigger_id } = req.body;
+                try {
+                    const result = await message.openCreateTeamDialog(trigger_id);
+                    if (result.data.error) {
+                        res.sendStatus(500);
+                    } else {
+                        res.send('');
                     }
+                } catch (err) {
+                    res.sendStatus(500);
                 }
-                if (empty) {
+            }
+        }
+        if (req.body.command === '/delete_team') {
+            if (!signature.isVerified(req)) {
+                res.sendStatus(404);
+                return;
+            } else {
+                const { user_id, trigger_id } = req.body;
+                try {
+                    const result = await message.openDeleteTeamDialog(trigger_id);
+                    if (result.data.error) {
+                        res.sendStatus(500);
+                    } else {
+                        res.send('');
+                    }
+                } catch (err) {
+                    res.sendStatus(500);
+                }
+            }
+        }
+        if (req.body.command === '/remove_user') {
+            if (!signature.isVerified(req)) {
+                res.sendStatus(404);
+                return;
+            } else {
+                const { user_id, trigger_id } = req.body;
+                try {
+                    const result = await message.openRemoveUserDialog(trigger_id);
+                    if (result.data.error) {
+                        res.sendStatus(500);
+                    } else {
+                        res.send('');
+                    }
+                } catch (err) {
+                    res.sendStatus(500);
+                }
+            }
+        }
+        if (req.body.command === '/add_user') {
+            if (!signature.isVerified(req)) {
+                res.sendStatus(404);
+                return;
+            } else {
+                const { user_id, trigger_id } = req.body;
+                try {
+                    const result = await message.openAddUserDialog(trigger_id);
+                    if (result.data.error) {
+                        res.sendStatus(500);
+                    } else {
+                        res.send('');
+                    }
+                } catch (err) {
+                    res.sendStatus(500);
+                }
+            }
+        }
+        if (req.body.command === '/list_teams') {
+            if (!signature.isVerified(req)) {
+                res.sendStatus(404);
+                return;
+            } else {
+                const { user_id, trigger_id } = req.body;
+                try {
+                    const result = await message.openListUsersOnTeamDialog(trigger_id);
+                    if (result.data.error) {
+                        res.sendStatus(500);
+                    } else {
+                        res.send('');
+                    }
+                } catch (err) {
+                    res.sendStatus(500);
+                }
+            }
+        }
+        if (req.body.command === '/update_team_position') {
+            if (!signature.isVerified(req)) {
+                res.sendStatus(404);
+                return;
+            } else {
+                const { user_id, trigger_id } = req.body;
+                try {
+                    const result = await message.openUpdateTeamDialog(trigger_id);
+                    if (result.data.error) {
+                        res.sendStatus(500);
+                    } else {
+                        res.send('');
+                    }
+                } catch (err) {
+                    res.sendStatus(500);
+                }
+            }
+        }
+        if (req.body.command === '/my_teams') {
+            if (!signature.isVerified(req)) {
+                res.sendStatus(404);
+                return;
+            } else {
+                const { user_id, trigger_id } = req.body;
+                try {
+                    const teamNames = await teamService.getMyTeamNames(user_id);
+                    message.sendShortMessage(user_id, `*Your teams are:*\n` +  teamNames.toString().replace(/[,]/g, "\n"));
+                    res.send('');
+                    console.log(result);
+                    if (result.data.error) {
+                        res.sendStatus(500);
+                    } else {
+                        res.send('');
+                    }
+                } catch (err) {
+                    res.sendStatus(500);
+                }
+            }
+        }
+        if (req.body.command === '/update_team_status') {
+            if (!signature.isVerified(req)) {
+                res.sendStatus(404);
+                return;
+            } else {
+                const { user_id, trigger_id } = req.body;
+                try {
                     message.sendShortMessage(user_id, `Nobody is here.`);
-                }
-                else {
-                    message.sendShortMessage(user_id, msgList.toString().replace(/[,]/g, '\n'));
-                }
-                res.send('');
-            } catch (e) {
-                console.log('error');
-                res.send(500);
-            }
-        }
-    }
-    if (req.body.command === '/in') {
-        if (!signature.isVerified(req)) {
-            res.sendStatus(404);
-            return;
-        } else {
-            const { user_id, trigger_id } = req.body;
-            try {
-                let result = await teamService.checkIfUserHasMultipleTeams(user_id);
-                if (result.multiple === 'false') {
-                    await teamService.updateTeamStatus(result.team_id, 'in');
-                    message.sendShortMessage(user_id, "*Team Status Set to:* \`in\`");
-                    res.send('');
-                }
-                else if (result.multiple === 'true') {
-                    try {
-                        const result = await message.openInDialog(trigger_id);
-                        if (result.data.error) {
-                            res.sendStatus(500);
-                        } else {
-                            res.send('');
-                        }
-                    } catch (err) {
+                    if (result.data.error) {
                         res.sendStatus(500);
+                    } else {
+                        res.send('');
                     }
+                } catch (err) {
+                    res.sendStatus(500);
                 }
-                else {
-                    message.sendShortMessage(user_id, '*Please register for a team by typing `/add_user`.*');
-                    res.send('');
-                }
-            } catch (e) {
-                console.log('error');
-                res.send(500);
             }
         }
-    }
-
-    if (req.body.command === '/out') {
-        if (!signature.isVerified(req)) {
-            res.sendStatus(404);
-            return;
-        } else {
-            const { user_id, trigger_id } = req.body;
-            try {
-                let result = await teamService.checkIfUserHasMultipleTeams(user_id);
-                if (result.multiple === 'false') {
-                    await teamService.updateTeamStatus(result.team_id, 'out');
-                    message.sendShortMessage(user_id, "*Team Status Set to:* \`out\`");
-                    res.send('');
-                }
-                else if (result.multiple === 'true') {
-                    try {
-                        const result = await message.openOutDialog(trigger_id);
-                        if (result.data.error) {
-                            res.sendStatus(500);
-                        } else {
-                            res.send('');
+        if (req.body.command === '`/who`s_here') {
+            if (!signature.isVerified(req)) {
+                res.sendStatus(404);
+                return;
+            } else {
+                const { user_id, trigger_id } = req.body;
+                try {
+                    let result = await teamService.getAllTeamsStatus();
+                    let empty = true;
+                    let msgList = [];
+                    msgList.push(`*Whos's here?*\n`);
+                    for (var a = 0; a < result.teams.length; a++) {
+                        if (result.teams[a].status =='1') {
+                            msgList.push(`\`${result.teams[a].team}\``);
+                            empty = false;
                         }
-                    } catch (err) {
-                        res.sendStatus(500);
                     }
-                }
-                else {
-                    message.sendShortMessage(user_id, '*Please register for a team by typing `/add_user`.*');
+                    if (empty) {
+                        message.sendShortMessage(user_id, `Nobody is here.`);
+                    }
+                    else {
+                        message.sendShortMessage(user_id, msgList.toString().replace(/[,]/g, '\n'));
+                    }
                     res.send('');
+                } catch (e) {
+                    console.log('error');
+                    res.send(500);
                 }
-            } catch (e) {
-                console.log('error');
-                res.send(500);
+            }
+        }
+        if (req.body.command === '/in') {
+            if (!signature.isVerified(req)) {
+                res.sendStatus(404);
+                return;
+            } else {
+                const { user_id, trigger_id } = req.body;
+                try {
+                    let result = await teamService.checkIfUserHasMultipleTeams(user_id);
+                    if (result.multiple === 'false') {
+                        await teamService.updateTeamStatus(result.team_id, 'in');
+                        message.sendShortMessage(user_id, "*Team Status Set to:* \`in\`");
+                        res.send('');
+                    }
+                    else if (result.multiple === 'true') {
+                        try {
+                            const result = await message.openInDialog(trigger_id);
+                            if (result.data.error) {
+                                res.sendStatus(500);
+                            } else {
+                                res.send('');
+                            }
+                        } catch (err) {
+                            res.sendStatus(500);
+                        }
+                    }
+                    else {
+                        message.sendShortMessage(user_id, '*Please register for a team by typing `/add_user`.*');
+                        res.send('');
+                    }
+                } catch (e) {
+                    console.log('error');
+                    res.send(500);
+                }
+            }
+        }
+
+        if (req.body.command === '/out') {
+            if (!signature.isVerified(req)) {
+                res.sendStatus(404);
+                return;
+            } else {
+                const { user_id, trigger_id } = req.body;
+                try {
+                    let result = await teamService.checkIfUserHasMultipleTeams(user_id);
+                    if (result.multiple === 'false') {
+                        await teamService.updateTeamStatus(result.team_id, 'out');
+                        message.sendShortMessage(user_id, "*Team Status Set to:* \`out\`");
+                        res.send('');
+                    }
+                    else if (result.multiple === 'true') {
+                        try {
+                            const result = await message.openOutDialog(trigger_id);
+                            if (result.data.error) {
+                                res.sendStatus(500);
+                            } else {
+                                res.send('');
+                            }
+                        } catch (err) {
+                            res.sendStatus(500);
+                        }
+                    }
+                    else {
+                        message.sendShortMessage(user_id, '*Please register for a team by typing `/add_user`.*');
+                        res.send('');
+                    }
+                } catch (e) {
+                    console.log('error');
+                    res.send(500);
+                }
             }
         }
     }
-    
-
-
-    
 }
 module.exports.run = function(req, res) {
     handleEvents(req, res);
