2167666c878967ab6f6aea8bd2372e3389421ff9
michaelvanbosch <mvanbosch@sourceallies.com>
2019-09-29 16:34:30 -0500

    why did i remove thisg

--- slack_api/src/controllers/commandsController.js
+++ slack_api/src/controllers/commandsController.js
@@ -102,7 +102,7 @@ let handleEvents = async function(req, res) {
         if (req.body.command === '/update_team_status') {
             const { user_id, trigger_id } = req.body;
             try {
-                message.sendShortMessage(user_id, `Nobody is here.`);
+                await message.openUpdateTeamDialog(trigger_id);
                 if (result.data.error) {
                     res.sendStatus(500);
                 } else {
